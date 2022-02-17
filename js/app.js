abcf80a7bd2b497df4e4d17a693669b9add0896a
Almas Khan <almas.rbm@gmail.com>
2022-02-17 13:11:37 +0600

    functionality done

--- js/app.js
+++ js/app.js
@@ -12,36 +12,56 @@ function getExpenses() {
     const clothesCost = parseFloat(clothesInput.value);
     //calculating tolat expenses
     const totalExpenses = foodCost + rentCost + clothesCost;
-
     const expensesField = document.getElementById('total-expenses');
     expensesField.innerText = totalExpenses;
 
     //clearing input field..
-    foodInput.value = '';
-    rentInput.value = '';
-    clothesInput.value = '';
+    // foodInput.value = '';
+    // rentInput.value = '';
+    // clothesInput.value = '';
     return totalExpenses;
+}
+function getIncome() {
+    const incomeInput = document.getElementById('income-input');
+    const income = parseFloat(incomeInput.value);
+    // //clearing input field
+    // incomeInput.value = '';
+    return income;
 }
 function getBalance() {
     //income
-    const incomeInput = document.getElementById('income-input');
-    const income = parseFloat(incomeInput.value);
+    const totalIncome = getIncome();
     //calculating balance
     const totalExpenses = getExpenses();
-    const balance = income - totalExpenses;
+    const balance = totalIncome - totalExpenses;
 
-    const balanceInput = document.getElementById('balance');
-    balanceInput.innerText = balance;
+    const balanceField = document.getElementById('balance');
+    balanceField.innerText = balance;
+    return balance;
+}
+function getSave() {
+    const saveInput = document.getElementById('save-input');
+    const save = parseFloat(saveInput.value);
+    const saveIncome = getIncome();
+    const savingAmount = (saveIncome * save) / 100;
 
-    //clearing input field
-    incomeInput.value = '';
+    const savingField = document.getElementById('saving-amount');
+    savingField.innerText = savingAmount;
+    return savingAmount;
+}
+function getRemainingBalance() {
+    const totalBalance = getBalance();
+    const totalSavingAmount = getSave();
+    const remainingBalance = totalBalance - totalSavingAmount;
+    const remainBalanceField = document.getElementById('remaining-balance');
+    remainBalanceField.innerText = remainingBalance;
 }
 
 //Calculate button
 document.getElementById('button-calculate').addEventListener('click', function () {
-    getBalance();
+    const balance = getBalance();
 })
 //Save Button
 document.getElementById('save-btn').addEventListener('click', function () {
-
+    const remainingBalance = getRemainingBalance();
 })
